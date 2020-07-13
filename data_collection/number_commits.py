9e51e9d910c0ccdc5c2198c7b07d722d0724cffd
Christopher Nguyen <chrisngyn99@gmail.com>
2020-07-13 12:20:34 -0400

    Added GraphQL example

--- data_collection/number_commits.py
+++ data_collection/number_commits.py
@@ -1,1 +1,16 @@
 # Found on Stack Overflow (in JavaScript) - https://stackoverflow.com/questions/18262288/finding-total-contributions-of-a-user-from-github-api
+# Example of using the GitHub API v4
+
+import requests
+import json
+
+query = """query {
+    viewer { 
+        login
+        name
+    }
+}"""
+
+url = "https://api.github.com/graphql"
+r = requests.post(url, headers={"Authorization": "Bearer TOKEN"}, json={"query": query})
+print(r.text)
