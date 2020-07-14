bd7f52a9effcc7844e96d5438906dc39c9a8bece
kbanc <katherine.bancroft@gmail.com>
2020-07-14 13:52:11 -0400

    Cleanup file and query

--- data_collection/most_worked_with.py
+++ data_collection/most_worked_with.py
@@ -6,16 +6,18 @@ from typing import Dict, List
 token = os.environ["GITHUB_API_TOKEN"]
 user = "kbanc"
 
-def update_ppl_worked_with(pull_request_contributions_nodes: List, ppl_worked_with: Dict):
+
+def update_ppl_worked_with(user: str, pull_request_contributions_nodes: List, ppl_worked_with: Dict):
     for pull_request in pull_request_contributions_nodes:
         for participant in pull_request["pullRequest"]["participants"]["nodes"]:
-            if participant["login"] != user: 
+            if participant["login"] != user:
                 if participant["login"] in ppl_worked_with:
                     ppl_worked_with[participant["login"]] += 1
                 else:
                     ppl_worked_with[participant["login"]] = 1
 
-def customize_query(user, cursor_str, num_users) -> str:
+
+def customize_query(user: str, cursor_str: str, num_users: int) -> str:
     return f"""
             query {{
             user(login:"{user}"){{
@@ -47,6 +49,7 @@ def customize_query(user, cursor_str, num_users) -> str:
             }}
         }}"""
 
+
 def get_people_worked_with(user: str) -> Dict:
     ppl_worked_with = {}
     cursor = None
@@ -62,16 +65,17 @@ def get_people_worked_with(user: str) -> Dict:
 
         new_total = num_users
         for pr in prs:
-            print('bool in python', pr["pullRequest"]["participants"]["pageInfo"]["hasNextPage"])
             if pr["pullRequest"]["participants"]["pageInfo"]["hasNextPage"]:
                 if pr["pullRequest"]["participants"]["totalCount"] > new_total:
                     new_total = pr["pullRequest"]["participants"]["totalCount"]+1
         if new_total > num_users:
             num_users = new_total
-            r = requests.post(url, headers={"Authorization": f"token {token}"}, json={"query": customize_query(user, cursor_str, num_users)})
+            r = requests.post(url, headers={"Authorization": f"token {token}"}, json={
+                              "query": customize_query(user, cursor_str, num_users)})
             response = json.loads(r.text)
 
-        update_ppl_worked_with(response["data"]["user"]["contributionsCollection"]["pullRequestContributions"]["nodes"], ppl_worked_with)
+        update_ppl_worked_with(user, response["data"]["user"]["contributionsCollection"]
+                               ["pullRequestContributions"]["nodes"], ppl_worked_with)
 
         pageInfo = response["data"]["user"]["contributionsCollection"]["pullRequestContributions"]["pageInfo"]
         if not pageInfo["hasNextPage"]:
@@ -81,4 +85,6 @@ def get_people_worked_with(user: str) -> Dict:
     return ppl_worked_with
 
 
-print(get_people_worked_with(user))
+people = get_people_worked_with(user)
+print("People worked with (desc order):", {k: v for k, v in sorted(
+    people.items(), key=lambda item: item[1], reverse=True)})
