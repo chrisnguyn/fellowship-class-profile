4dd1812feba84010c39acfde81d74c34f3b1dd72
kbanc <katherine.bancroft@gmail.com>
2020-07-14 09:12:55 -0400

    Clean up formatting

--- data_collection/most_worked_on.py
+++ data_collection/most_worked_on.py
@@ -5,50 +5,55 @@ import requests
 token = os.environ["GITHUB_API_TOKEN"]
 user = "kbanc"
 
-num_repos = 3
+num_repos = 5
 num_lang = 2
 
-query = f"""query {{
-  user(login:"{user}"){{
-    contributionsCollection(from: "2020-06-01T07:00:00Z", to:"2020-08-24T07:00:00Z" ) {{
-			totalRepositoriesWithContributedPullRequests
-      totalPullRequestContributions
-      totalCommitContributions
-      totalIssueContributions
-      popularPullRequestContribution {{
-        pullRequest {{
-          title
-          url
-          merged
-          repository{{
+query = f"""
+  query {{
+    user(login:"{user}"){{
+      contributionsCollection(from: "2020-06-01T07:00:00Z", to:"2020-08-24T07:00:00Z" ) {{
+        totalRepositoriesWithContributedPullRequests
+        totalPullRequestContributions
+        totalCommitContributions
+        totalIssueContributions
+        popularPullRequestContribution {{
+          pullRequest {{
+            title
+            url
+            merged
+            repository{{
+              name
+              id
+              languages(first:1){{
+                nodes {{
+                  name
+                }}
+              }}
+            }}
+          }}
+        }}
+        pullRequestContributionsByRepository(maxRepositories: {num_repos}){{
+          repository {{
             name
-            id
-						languages(first:1){{
-              nodes {{
+            languages(first: {num_lang}){{
+              nodes{{
                 name
               }}
             }}
           }}
         }}
       }}
-      pullRequestContributionsByRepository(maxRepositories: {num_repos}){{
-        repository {{
-          name
-          languages(first: {num_lang}){{
-            nodes{{
-              name
-            }}
-          }}
-        }}
-      }}
     }}
-  }}
-}}"""
+  }}"""
 
 url = "https://api.github.com/graphql"
-r = requests.post(url, headers={"Authorization": f"token {token}"}, json={"query": query})
+r = requests.post(
+    url, headers={"Authorization": f"token {token}"}, json={"query": query})
 
 response = json.loads(r.text)
-print(f"Top {num_repos} contributed to repo:", response["data"]["user"]["contributionsCollection"]["pullRequestContributionsByRepository"])
-print("Total number of PRs during Fellowship (to any repo)", response["data"]["user"]["contributionsCollection"]["totalPullRequestContributions"])
-print("PR with the most discussion", response["data"]["user"]["contributionsCollection"]["popularPullRequestContribution"])
+print(f"Top {num_repos} contributed to repo:",
+      response["data"]["user"]["contributionsCollection"]["pullRequestContributionsByRepository"])
+print("Total number of PRs during Fellowship (to any repo)",
+      response["data"]["user"]["contributionsCollection"]["totalPullRequestContributions"])
+print("PR with the most discussion",
+      response["data"]["user"]["contributionsCollection"]["popularPullRequestContribution"])
