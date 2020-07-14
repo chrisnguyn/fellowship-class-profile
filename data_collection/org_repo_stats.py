5ab72c499510ba38412a09237a061fb2eb70379c
kbanc <katherine.bancroft@gmail.com>
2020-07-14 10:24:41 -0400

    Refactor and cleanup duplicate code

--- data_collection/org_repo_stats.py
+++ data_collection/org_repo_stats.py
@@ -1,60 +1,54 @@
 import json
 import os
 import requests
+from typing import Dict
 
 token = os.environ["GITHUB_API_TOKEN"]
-user = "kbanc"
-
-
-num_original_repo_query = """
-query {
-    organization(login: "MLH-Fellowship"){
-        repositories(isFork: false){
-            totalCount
-        }
-    }
-}
-"""
-num_forked_repo_query = """
-query {
-    organization(login: "MLH-Fellowship"){
-        repositories(isFork: true){
-            totalCount
-        }
-    }
-}
-"""
-
 url = "https://api.github.com/graphql"
-r = requests.post(
-    url, headers={"Authorization": f"token {token}"}, json={"query": num_forked_repo_query})
 
-num_forked_repo = json.loads(r.text)["data"]["organization"]["repositories"]["totalCount"]
 
-r = requests.post(
-    url, headers={"Authorization": f"token {token}"}, json={"query": num_original_repo_query})
-num_original_repo = json.loads(r.text)["data"]["organization"]["repositories"]["totalCount"]
-
-repo_info_query = f"""
-query {{ 
-    organization(login: "MLH-Fellowship"){{
-        repositories(first: {num_forked_repo}, isFork: true) {{
-            totalCount
-            nodes {{
-                name
-                parent{{
-                    nameWithOwner
-                }}
-                primaryLanguage{{
-                    name
-                }}
-                homepageUrl
-                url
+def get_number_repos_in_MLH(isFork: str = "true") -> int:
+    num_repo_query = f"""
+    query {{
+        organization(login: "MLH-Fellowship"){{
+            repositories(isFork: {isFork}){{
+                totalCount
             }}
         }}
     }}
-}}"""
+    """
+    r = requests.post(
+        url, headers={"Authorization": f"token {token}"}, json={"query": num_repo_query})
+    return json.loads(r.text)["data"]["organization"]["repositories"]["totalCount"]
 
-r = requests.post(
-    url, headers={"Authorization": f"token {token}"}, json={"query": repo_info_query})
-repo_info = json.loads(r.text)["data"]["organization"]["repositories"]["nodes"]
+
+def get_all_forked_repos_in_MLH() -> Dict:
+    num_forked_repo = get_number_repos_in_MLH(isFork=True)
+    repo_info_query = f"""
+    query {{ 
+        organization(login: "MLH-Fellowship"){{
+            repositories(first: {num_forked_repo}, isFork: true) {{
+                totalCount
+                nodes {{
+                    name
+                    parent{{
+                        nameWithOwner
+                    }}
+                    primaryLanguage{{
+                        name
+                    }}
+                    homepageUrl
+                    url
+                }}
+            }}
+        }}
+    }}"""
+
+    r = requests.post(
+        url, headers={"Authorization": f"token {token}"}, json={"query": repo_info_query})
+    return json.loads(r.text)["data"]["organization"]["repositories"]["nodes"]
+
+
+num_original_repo = get_number_repos_in_MLH(isFork="false")
+num_forked_repo = get_number_repos_in_MLH(isFork="true")
+num_repos = get_all_forked_repos_in_MLH()
