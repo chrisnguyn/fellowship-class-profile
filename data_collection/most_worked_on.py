436c9f85c87e3cb04e9a8b0212075a880ac59026
kbanc <katherine.bancroft@gmail.com>
2020-07-15 09:52:42 -0400

    Made fn for most_worked_on

--- data_collection/most_worked_on.py
+++ data_collection/most_worked_on.py
@@ -3,11 +3,8 @@ import requests
 
 from general.static import end_date, endpoints, start_date
 from general.user import username, user_token
+from typing import Dict
 from org_repo_stats import get_all_forked_repos_in_MLH
-
-
-num_repos = 5
-num_lang = 2
 
 
 def get_repos_in_MLH_project_list(repositories):
@@ -19,49 +16,53 @@ def get_repos_in_MLH_project_list(repositories):
     return mlh_project_repos
 
 
-query = f"""
-  query {{
-    user(login:"{username}"){{
-      contributionsCollection(from: "{start_date}", to:"{end_date}" ) {{
-        totalRepositoriesWithContributedPullRequests
-        totalPullRequestContributions
-        totalCommitContributions
-        totalIssueContributions
-        popularPullRequestContribution {{
-          pullRequest {{
-            title
-            url
-            merged
-            repository{{
-              name
-              id
-              languages(first:1){{
-                nodes {{
-                  name
+def get_most_worked_on(user: str) -> Dict:
+    num_repos = 5
+    query = f"""
+    query {{
+      user(login:"{user}"){{
+        contributionsCollection(from: "{start_date}", to:"{end_date}" ) {{
+          totalRepositoriesWithContributedPullRequests
+          totalPullRequestContributions
+          totalCommitContributions
+          totalIssueContributions
+          popularPullRequestContribution {{
+            pullRequest {{
+              title
+              url
+              merged
+              repository{{
+                name
+                id
+                languages(first:1){{
+                  nodes {{
+                    name
+                  }}
                 }}
               }}
             }}
           }}
-        }}
-        pullRequestContributionsByRepository(maxRepositories: {num_repos}){{
-          repository {{
-            name
-            languages(first: {num_lang}){{
-              nodes{{
-                name
-              }}
+          pullRequestContributionsByRepository(maxRepositories: {num_repos}){{
+            repository {{
+              	name
+				primaryLanguage {{
+					name
+				}}
             }}
           }}
         }}
       }}
-    }}
-  }}"""
+    }}"""
+    r = requests.post(
+        endpoints["github"], headers={"Authorization": f"token {user_token}"}, json={"query": query})
 
-r = requests.post(
-    endpoints["github"], headers={"Authorization": f"token {user_token}"}, json={"query": query})
+    print(json.loads(r.text))
 
-response = json.loads(r.text)
-print(f"Top {num_repos} contributed to repo:",
+    return json.loads(r.text)
+
+
+response = get_most_worked_on(user="kbanc")
+print(f"Top contributed to repos:",
       response["data"]["user"]["contributionsCollection"]["pullRequestContributionsByRepository"])
 
 print("Top repos that were part of the MLH open source projects:", get_repos_in_MLH_project_list(
