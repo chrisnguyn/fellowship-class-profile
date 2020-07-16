be58a3af53a91c3120183dfb8289984ab6f1ea7f
Yanlam Ko <43295248+YKo20010@users.noreply.github.com>
2020-07-16 15:06:29 -0400

    Add pod mates on personal page (#31)
    * Setup web files
    
    * Setup navbar and personal vs. class pages
    
    * Setup donut charts
    
    * Add redirect for navbar
    
    * Move donutChart to separate file
    
    * Update personal page
    
    * Setup contributions over time graph
    
    * Add contributions chart
    
    * Add .env to .gitignore
    
    * Add pod
    
    Co-authored-by: kbanc <katherine.bancroft@gmail.com>

---
+++ web/static/javascript/teamCircle.js
@@ -0,0 +1,83 @@
+function renderTeamCircle(body, team_data, username) {
+    body.append("div")
+        .attr("class", "subheader")
+        .style("color", colors.black)
+        .html("Team")
+    let div = body.append("div")
+        .style("display", "flex")
+        .style("align-items", "center")
+        .style("justify-content", "center")
+    let svg = div.append("svg")
+        .attr("height", 500)
+        .attr("width", 900)
+
+    let height = svg.attr("height")
+    let width = svg.attr("width")
+
+    let team = Object.keys(team_data).find(team => {
+        return team_data[team].includes(username)
+    })
+
+    let angleScale = d3.scaleLinear()
+        .domain([0, team_data[team].length])
+        .range([0, 2 * Math.PI])
+
+    let radius = {
+        pod: team.length * 4 + 4,
+        memberRing: Math.min(height, width) - 400,
+        member: 10
+    }
+
+    svg.append("circle")
+        .attr("cx", width / 2)
+        .attr("cy", height / 2)
+        .attr("r", radius.pod)
+        .attr("fill", colors.grey)
+    svg.append("circle")
+        .attr("cx", width / 2)
+        .attr("cy", height / 2)
+        .attr("r", radius.pod + 4)
+        .attr("fill", "none")
+        .attr("stroke", colors.grey)
+        .attr("stroke-width", 2)
+    svg.append("circle")
+        .attr("cx", width / 2)
+        .attr("cy", height / 2)
+        .attr("r", radius.memberRing)
+        .attr("fill", "none")
+        .attr("stroke", colors.grey)
+        .attr("stroke-width", 2)
+    svg.append("text")
+        .attr("x", width / 2)
+        .attr("y", height / 2)
+        .attr("font-family", "Space Mono")
+        .attr("font-size", 12)
+        .attr("text-anchor", "middle")
+        .attr("alignment-baseline", "middle")
+        .attr("fill", colors.black)
+        .text(team)
+
+    team_data[team].forEach((member, i) => {
+        let angle = angleScale(i)
+        let x = width / 2 + radius.memberRing * Math.cos(angle)
+        let y = height / 2 + radius.memberRing * Math.sin(angle)
+        svg.append("circle")
+            .attr("cx", x)
+            .attr("cy", y)
+            .attr("r", radius.member)
+            .attr("fill", colors.grey)
+        x += (radius.member + 12) * Math.cos(angle)
+        y += (radius.member + 12) * Math.sin(angle)
+        angle = angle * 180 / Math.PI + 75
+
+        svg.append("text")
+            .attr("class", "caption")
+            .attr("x", x)
+            .attr("y", y)
+            .attr("text-anchor", (angle > 90 && angle < 270) ? "end" : "start")
+            .attr("alignment-baseline", "middle")
+            .attr("fill", colors.black)
+            .attr("transform", "rotate (" + ((angle > 90 && angle < 270) ? angle + 180 : angle) + " " + x + " " + y + ")")
+            .text(member)
+    })
+}
