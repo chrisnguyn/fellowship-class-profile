a26cf4d314d776069db37bf2c321451641146613
Christopher Nguyen <chrisngyn99@gmail.com>
2020-07-16 22:27:31 -0400

    Updated cloud.js to use fetched data

--- web/static/javascript/cloud.js
+++ web/static/javascript/cloud.js
@@ -1,30 +1,26 @@
 anychart.onDocumentReady(function() {
     let display_data = []
-    let org_stats = {  // dummy data
-        "None": 3,
-        "JavaScript": 34,
-        "TypeScript": 9,
-        "Julia": 19,
-        "Python": 14,
-        "Jupyter Notebook": 2,
-        "C": 1,
-        "C++": 2,
-        "Shell": 4,
-        "Ruby": 3,
-        "HTML": 3,
-        "Java": 2
-    }
+    let org_statistics = fetch("https://fellowship-class-profile.herokuapp.com/global_stats")
+                         .then(response => response.json())
+                         .then(json_response => json_response["repo_lang_stats"])
+                         .then(data => generate_word_cloud(data))
 
-    for (let key in org_stats) {
-        let val = org_stats[key];
-        display_data.push({"x": `${key}`, "value": `${val}`, category: `${key}`})  // {"x": "howdoi", "value": 10, category: "Python"}
-    }
+    function generate_word_cloud(org_stats) {
+        for (let key in org_stats) {
+            if (key === "TOTAL NUMBER REPOS" || key === "None") {
+                continue;
+            }
 
-    let chart = anychart.tagCloud(display_data);
-    chart.title("MLH-Fellowship Repository Language Popularity");
-    chart.angles([0])
-    chart.colorRange(true);
-    chart.colorRange().length('80%');
-    chart.container("container");
-    chart.draw();
+            let val = org_stats[key];
+            display_data.push({"x": `${key}`, "value": `${val}`, category: `${key}`})
+        }
+    
+        let chart = anychart.tagCloud(display_data);
+        chart.title("MLH Fellowship Repository Language Popularity");
+        chart.angles([0])
+        chart.colorRange(true);
+        chart.colorRange().length('80%');
+        chart.container("container");
+        chart.draw();
+    }
 });
