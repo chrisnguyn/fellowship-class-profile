420481560d01b69e14d285e3cfd0288f7ed8cc20
Chris N <chrisngyn99@gmail.com>
2020-07-17 11:44:01 -0400

    Word cloud (#39)
    * Word cloud
    
    * Updated cloud.js to use fetched data
    
    * added personal cloud and global cloud

---
+++ web/static/javascript/globalCloud.js
@@ -0,0 +1,28 @@
+anychart.onDocumentReady(function() {
+    let display_data = []
+    let org_statistics = fetch("https://fellowship-class-profile.herokuapp.com/global_stats")
+                         .then(response => response.json())
+                         .then(json_response => json_response["repo_lang_stats"])
+                         .then(data => generate_word_cloud(data))
+
+    function generate_word_cloud(org_stats) {
+        console.log(org_stats)
+
+        for (let key in org_stats) {
+            if (key === "TOTAL NUMBER REPOS" || key === "None") {
+                continue;
+            }
+
+            let val = org_stats[key];
+            display_data.push({"x": `${key}`, "value": `${val}`, category: `${key}`})
+        }
+    
+        let chart = anychart.tagCloud(display_data);
+        chart.title("MLH Fellowship Repository Language Popularity");
+        chart.angles([0])
+        chart.colorRange(true);
+        chart.colorRange().length('80%');
+        chart.container("container");
+        chart.draw();
+    }
+});
